0666e48d7595abc2a93a9e4d133f37ac4040b944
g1kwon <spring_shot@naver.com>
2022-08-09 21:47:54 +0900

    spring proxyfactory - introduce using jdkDynamicProxy with proxyfactory

---
+++ src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
@@ -0,0 +1,32 @@
+package hello.proxy.proxyfactory;
+
+import hello.proxy.common.advice.TimeAdvice;
+import hello.proxy.common.service.ServiceImpl;
+import hello.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.AopUtils;
+
+@Slf4j
+public class ProxyFactoryTest {
+
+    @Test
+    @DisplayName("인터페이스가 있으면 JDK 동적 프록시 적용")
+    void interfaceProxy() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvice(new TimeAdvice());
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        proxy.save();
+
+        Assertions.assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        Assertions.assertThat(AopUtils.isJdkDynamicProxy(proxy)).isTrue();
+        Assertions.assertThat(AopUtils.isCglibProxy(proxy)).isFalse();
+    }
+}
