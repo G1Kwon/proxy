5c59040b350c1556c1cd36d30b4e7dcc14302592
g1kwon <spring_shot@naver.com>
2022-08-15 23:19:55 +0900

    beanPostProcessor example

---
+++ src/test/java/hello/proxy/postprocessor/BeanPostProcessorTest.java
@@ -0,0 +1,63 @@
+package hello.proxy.postprocessor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanPostProcessorTest {
+
+    @Test
+    void basicConfig() {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BasicConfig.class);
+
+        //A는 빈으로 등록된다.
+        A a = applicationContext.getBean("beanA", A.class);
+        a.helloA();
+
+        //B는 빈으로 등록되지 않는다.
+        Assertions.assertThrows(NoSuchBeanDefinitionException.class, () -> applicationContext.getBean(B.class));
+    }
+
+    @Slf4j
+    @Configuration
+    static class BasicConfig {
+        @Bean(name = "beanA")
+        public A a() {
+            return new A();
+        }
+    }
+
+    @Slf4j
+    static class A {
+        public void helloA() {
+            log.info("hello A");
+        }
+    }
+
+    @Slf4j
+    static class B {
+        public void helloB() {
+            log.info("hello B");
+        }
+    }
+
+    @Slf4j
+    static class AtoBPostProcessor implements BeanPostProcessor {
+
+        @Override
+        public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+            log.info("beanName={} bean={}", beanName, bean);
+            if (bean instanceof A) {
+                return new B();
+            }
+            return bean;
+        }
+    }
+}
